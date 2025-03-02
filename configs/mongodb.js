ba63e311d35d573138e5d2fb015b7b5fb3309c79
nelson <chinwubanelson@gmail.com>
2025-03-02 11:28:51 +0100

    change db name

--- configs/mongodb.js
+++ configs/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/neldemylms`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/Neldemy-LMS`);
   
 };
 export default connectDB;
